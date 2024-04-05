73f2497d26450f23ca7bca2774f9d73e554fe31b
jczsk <zuodaoyitian@gmail.com>
2024-04-05 15:02:18 +0800

    Site updated: 2024-04-05 15:02:17

--- js/ua.js
+++ js/ua.js
@@ -25,22 +25,23 @@ if(sessionStorage.os_name == null){
                   });
     }; 
     document.getElementsByTagName('head')[0].appendChild(uaparser);
-    function getPCNum(){var agent=navigator.userAgent.toLowerCase();var isMac=function(){return/macintosh|mac os x/i.test(navigator.userAgent)}();if(agent.indexOf("win32")>=0||agent.indexOf("wow32")>=0){return "X86"}if(agent.indexOf("win64")>=0||agent.indexOf("wow64")>=0){return "X64"}}
 ua();
 }else{
-    function ua() {
-      let cpu = document.getElementById("cpu");
-          cpu.innerText=sessionStorage.cpu_architecture;
-          cpu.href="/cpu/"+sessionStorage.cpu_architecture.toLowerCase().replace(/\s+/g, '')+".html";
-      let device = document.getElementById("device");
-          device.innerText=sessionStorage.device_vendor+" "+sessionStorage.device_model;
-          device.href="/device/"+sessionStorage.device_vendor.toLowerCase().replace(/\s+/g, '')+".html";
-      let os = document.getElementById("os");
-          os.innerText=sessionStorage.os_name+" "+sessionStorage.os_version+" "+sessionStorage.os_num;
-          os.href="/os/"+sessionStorage.os_name.toLowerCase().replace(/\s+/g, '')+".html";
-      let browser = document.getElementById("browser");
-          browser.innerText=sessionStorage.browser_name+" "+sessionStorage.browser_version;
-          browser.href="/browser/"+sessionStorage.browser_name.toLowerCase().replace(/\s+/g, '')+".html";
-    }
     ua();
 }
+
+function ua() {
+    let cpu = document.getElementById("cpu");
+        cpu.innerText=sessionStorage.cpu_architecture;
+        cpu.href="/cpu/"+sessionStorage.cpu_architecture.toLowerCase().replace(/\s+/g, '')+".html";
+    let device = document.getElementById("device");
+        device.innerText=sessionStorage.device_vendor+" "+sessionStorage.device_model;
+        device.href="/device/"+sessionStorage.device_vendor.toLowerCase().replace(/\s+/g, '')+".html";
+    let os = document.getElementById("os");
+        os.innerText=sessionStorage.os_name+" "+sessionStorage.os_version+" "+sessionStorage.os_num;
+        os.href="/os/"+sessionStorage.os_name.toLowerCase().replace(/\s+/g, '')+".html";
+    let browser = document.getElementById("browser");
+        browser.innerText=sessionStorage.browser_name+" "+sessionStorage.browser_version;
+        browser.href="/browser/"+sessionStorage.browser_name.toLowerCase().replace(/\s+/g, '')+".html";
+  }
+  function getPCNum(){var agent=navigator.userAgent.toLowerCase();var isMac=function(){return/macintosh|mac os x/i.test(navigator.userAgent)}();if(agent.indexOf("win32")>=0||agent.indexOf("wow32")>=0){return "X86"}if(agent.indexOf("win64")>=0||agent.indexOf("wow64")>=0){return "X64"}}
