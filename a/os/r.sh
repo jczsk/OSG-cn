1a390ef933ec69652b96cdddc5f62305a88e3090
jczsk <zuodaoyitian@gmail.com>
2024-08-28 10:59:03 +0800

    Site updated: 2024-08-28 10:58:44

---
+++ a/os/r.sh
@@ -0,0 +1,37 @@
+#!/bin/bash  
+
+if [ ! -f "lists.txt" ]; then  
+    echo "Error: lists.txt file not found."  
+    exit 1  
+fi  
+   
+while IFS= read -r line; do  
+
+    trimmed_line=$(echo "$line" | sed 's/ *$//' | tr -d '[:space:]' | tr A-Z a-z)  
+    title_line=$(echo "$line" | sed 's/ *$//' )
+    if [ -n "$trimmed_line" ]; then  
+        touch "${trimmed_line}.md"
+        echo "---
+lang: zh-CN
+title: "${title_line} 操作系统指南"
+description: ${title_line}
+category: 
+    - 操作系统
+tag: 
+Date: 
+star: 
+sticky: 
+---
+
+<div class='ua-list'></div>
+
+## ${title_line}
+
+" > "${trimmed_line}.md"  
+        echo "Created file: ${trimmed_line}.md"  
+    fi  
+done < lists.txt  
+  
+
+
+echo "All .md files have been created successfully."
